cf5968ec12b6e1a77d696f0c7ca2114522abea4d
Trần Vĩnh Phúc <70811800+Pill50@users.noreply.github.com>
2022-04-30 21:16:33 +0700

    Create 1431.cpp

---
+++ cpp/array/1431.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int max = -1;
+        vector<bool> ans;
+        for(int i = 0; i < candies.size(); i++) {
+            if(candies[i] > max) max = candies[i];
+        }
+        for(int i = 0; i < candies.size(); i++) {
+            if(candies[i] + extraCandies >= max) ans.push_back(true);
+            else ans.push_back(false);
+        }
+        return ans;
+    }
+};
