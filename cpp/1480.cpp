192032a9251aa5418539eb452c5b76bb56c46709
Trần Vĩnh Phúc <70811800+Pill50@users.noreply.github.com>
2022-04-30 21:11:23 +0700

    Delete cpp directory

--- cpp/1480.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    vector<int> runningSum(vector<int>& nums) {
-        int l = nums.size();
-        for(int i = 1; i < l; ++i)
-            nums[i] += nums[i-1];
-        return nums;
-    }
-};
