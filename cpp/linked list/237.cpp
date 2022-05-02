5673d439657786f7c569f8409ff01b3c79eaf6f5
Trần Vĩnh Phúc <70811800+Pill50@users.noreply.github.com>
2022-05-03 01:03:15 +0700

    Create 237.cpp

---
+++ cpp/linked list/237.cpp
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* temp = node;
+        temp->val = node->next->val;
+        temp->next = node->next->next;
+    }
+};
